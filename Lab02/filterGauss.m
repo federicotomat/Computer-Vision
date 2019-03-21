3dad90189fb71518031f3a844cc73f3687983a83
Federico Tomat <39094212+federicotomat@users.noreply.github.com>
2019-03-21 17:28:17 +0100

    Update filterGauss.m

--- Lab02/filterGauss.m
+++ Lab02/filterGauss.m
@@ -1,13 +1,13 @@
 %% Gaussian filter
 % Remove the noise by using a low-pass Gaussian filter
 
-function imgFilterGauss = filterGauss(srcImg, sizeH)
+function imgFilterGauss = filterGauss(inputImg, sizeH)
     % The half of the filter size must be three times the standard deviation
     sigma = sizeH/6;
     % Create a Gaussian filter
     H = fspecial('gaussian', sizeH, sigma);
-    figure,imagesc(H),title(['Gaussian filter image space size ', sprintf('%d',sizeH)])
-    figure,surf(H),title(['Gaussian filter surface space size ', sprintf('%d',sizeH);])
+    figure, imagesc(H), title(['Gaussian filter image space size ', sprintf('%d',sizeH)])
+    figure, surf(H), title(['Gaussian filter surface space size ', sprintf('%d',sizeH);])
     % Applying filter to the image
-    imgFilterGauss = imfilter(srcImg, H);
+    imgFilterGauss = imfilter(inputImg, H);
 end
