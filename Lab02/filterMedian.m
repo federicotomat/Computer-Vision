0f5ff1e40af185156877e541fe8967efe02fca7f
Federico Tomat <39094212+federicotomat@users.noreply.github.com>
2019-03-21 14:51:42 +0100

    Push

--- Lab02/filterMedian.m
+++ Lab02/filterMedian.m
@@ -2,10 +2,5 @@
 % Remove the noise by using a median filter
 
 function imgMedianFilter = filterMedian(srcImg, sizeM)
-<<<<<<< HEAD
     imgMedianFilter = medfilt2(srcImg,[sizeM,sizeM]);
 end
-=======
-imgMedianFilter = medfilt2(srcImg,[sizeM,sizeM]);
-end
->>>>>>> 66ac33d5f406e071f7290ab2e1e522f01bdf1033
--- Lab02/filterMovingAverage.m
+++ Lab02/filterMovingAverage.m
@@ -1,19 +1,13 @@
 %% Moving average
 % Remove the noise by using a moving average
 
-<<<<<<< HEAD
-function imgFilterAve = filterMovingAverage(srcImg, sizeK)
-    K = ones(sizeK)/(sizeK^2); % kernel to make the average between neighborhood pixels
-    imgFilterAve = conv2(srcImg,K,'same'); % 2D convolution
-=======
 function imgFilterAve = filterMovingAverage(inputImg, sizeK) 
-%costruction of the agumented matrix to ignore the fact that we are
-%loosing with convolution
-inputImg=matrixLedger(inputImg, sizeK);
-% kernel to make the average between neighborhood pixels
-K = ones(sizeK)/(sizeK^2);
-% 2D convolution
-imgFilterAve = conv2(inputImg, K, 'same');
-imgFilterAve=imgFilterAve(floor(sizeK/2):(end-floor(sizeK/2)),floor(sizeK/2):(end-floor(sizeK/2)));
->>>>>>> 66ac33d5f406e071f7290ab2e1e522f01bdf1033
+    %costruction of the agumented matrix to ignore the fact that we are
+    %loosing with convolution
+    inputImg=matrixLedger(inputImg, sizeK);
+    % kernel to make the average between neighborhood pixels
+    K = ones(sizeK)/(sizeK^2);
+    % 2D convolution
+    imgFilterAve = conv2(inputImg, K, 'same');
+    imgFilterAve=imgFilterAve(floor(sizeK/2):(end-floor(sizeK/2)),floor(sizeK/2):(end-floor(sizeK/2)));
 end
