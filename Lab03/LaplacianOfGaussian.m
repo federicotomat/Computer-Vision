1cbb6fa3b28e08999a98aceef2e33787ce46c740
Giovanni <giovaborr@gmail.com>
2019-04-02 13:04:23 +0200

    ciaooo

---
+++ Lab03/LaplacianOfGaussian.m
@@ -0,0 +1,12 @@
+%%  Laplacian of Gaussian Operator
+% Function that implements the Laplacian of Gaussian Operator with a given
+% standard deviation sigma
+
+function laplacianMatrix = LaplacianOfGaussian(sigma)
+%Spatial support should be the nearesr integer to 3*sigma
+
+spatialSupport = ceil(sigma*3); 
+[X,Y]=meshgrid(-spatialSupport:spatialSupport);
+laplacianMatrix = (1/(2*pi*sigma^2))*((X.^2+Y.^2 -2*sigma^2)/sigma^4).*exp(-(X.^2+Y.^2)/(2*sigma^2));
+
+end
