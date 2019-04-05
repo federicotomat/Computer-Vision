d1258111c259a7b703451858ed8cf0c19abaac1a
Federico Tomat <39094212+federicotomat@users.noreply.github.com>
2019-04-05 14:53:30 +0200

    Add find method, ma meglio i for

--- Lab03/include/autoThreshold.m
+++ Lab03/include/autoThreshold.m
@@ -1,7 +1,7 @@
 %% Auto Threshold generate function
 
 function threshold = autoThreshold(imgInput)
-     C = 0.25;
+     C = 0.1;
      ThreshLow = max(1,C*(mean2(imgInput)-std2(imgInput)))/255;
      ThreshHigh = min(254,C*(mean2(imgInput)+std2(imgInput)))/255;
      threshold = [ThreshLow ThreshHigh];
