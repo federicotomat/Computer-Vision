e9dc7d2cce484e34ec385fd8427315747e3da51e
Federico Tomat <39094212+federicotomat@users.noreply.github.com>
2019-04-08 13:16:55 +0200

    Change print figure, and fixes

--- Lab03/include/errorAnalisys.m
+++ Lab03/include/errorAnalisys.m
@@ -1,6 +1,6 @@
 %% Error Analisis
 
-function error_percentage = errorAnalisys(mat1, mat2)
+function errorAnalisys(mat1, mat2)
     if size(mat1) ~= size(mat2) 
         msg='The two images need to have the same size';
         error(msg)
@@ -16,6 +16,6 @@ function error_percentage = errorAnalisys(mat1, mat2)
     end
 
     error_percentage = error_count/(size(mat1,2)*size(mat1,1))*100;
-    disp(error_percentage)
-    figure, imshowpair(mat1, mat2);
+    % disp(error_percentage)
+    imshowpair(mat1, mat2), title(strcat('Similarity error =', {' '}, num2str(error_percentage)));
 end
