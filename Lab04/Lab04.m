edc9d8863982d733bfa7562f36acff48d9b2766c
Giovanni <giovaborr@gmail.com>
2019-04-09 14:05:55 +0200

    set lab 04

--- Lab04/Lab04.m
+++ Lab04/Lab04.m
@@ -5,22 +5,49 @@ clear all
 clc
 close all
 
-imgRgb(:,:,:,1) = imread('ur_c_s_03a_01_L_0376.png');
-imgRgb(:,:,:,2) = imread('ur_c_s_03a_01_L_0377.png');
-imgRgb(:,:,:,3) = imread('ur_c_s_03a_01_L_0378.png');
-imgRgb(:,:,:,4) = imread('ur_c_s_03a_01_L_0379.png');
-imgRgb(:,:,:,5) = imread('ur_c_s_03a_01_L_0380.png');
-imgRgb(:,:,:,6) = imread('ur_c_s_03a_01_L_0381.png');
-
-for i=1:6 % Applying colorMaps to the six images
-    [chHue(:,:,i), imgGray(:,:,i)] = colorMaps(imgRgb(:,:,:,i));
+%% Load image
+for i = 1:6
+name = strcat('ur_c_s_03a_01_L_0', num2str(375+1),'.png');
+imgRGB(:,:,:,i) = imread(name);
 end
 
-imageNumber = 1; % Select the image to recognize the red car (1= {...}0376.png)
-method = 2; % Select method with different threshold
-sizeFilter = 9; % Select average filter size
-mask = maskHue(chHue(:,:,imageNumber), method, sizeFilter);
-figure,imagesc(mask),colormap gray, title(['Final mask with method ' num2str(method)])
+%% RGB and HSI
 
-minClusterArea = 200; % Select a minimum for the cluster areas
-detectCar(mask, imgGray(:,:,imageNumber), minClusterArea, method);
+for i=1:6
+    [imgHSI(:,:,:,i), imgray(:,:,i)] = colorMaps(imgRGB(:,:,:,i));
+    title{i} = {['Hue for image number ' num2str(375+i)], ['Saturation for image number ' num2str(375+i)], ...
+               ['Value for image number ' num2str(375+i)], ['Red for image number ' num2str(375+i)],...
+              ['Green for image number ' num2str(375+i)], ['Blue for image number ' num2str(375+i)]};
+    printFigure(6, 3, {imgHSI(:,:,1,i),imgHSI(:,:,2,i),imgHSI(:,:,3,i),...
+        imgRGB(:,:,1,i),imgRGB(:,:,2,i),imgRGB(:,:,3,i)} , {1 1 1 1 1 1}, title{i}, 1)
+    
+end
+    
+for i=1:3
+% Hue-Saturation-Value
+ 
+printFigure(6, 3, {imgHSI(:,:,i,1),imgHSI(:,:,i,2),imgHSI(:,:,i,3),...
+        imgHSI(:,:,i,4),imgHSI(:,:,i,5),imgHSI(:,:,i,6)} , {1 1 1 1 1 1}, {title{1}{i}, title{2}{i},title{3}{i}, title{4}{i},title{5}{i},title{6}{i}}, 1);
+
+    
+% Red-Green-Blue
+ 
+printFigure(6, 3, {imgRGB(:,:,i,1),imgRGB(:,:,i,2),imgRGB(:,:,i,3),...
+        imgRGB(:,:,i,4),imgRGB(:,:,i,5),imgRGB(:,:,i,6)} , {1 1 1 1 1 1}, {title{1}{3+i}, title{2}{3+i},title{3}{3+i},title{4}{3+i},title{5}{3+i},title{6}{3+i}}, 1);
+
+end
+
+
+
+
+%% 
+
+
+% imageNumber = 1; % Select the image to recognize the red car (1= {...}0376.png)
+% method = 2; % Select method with different threshold
+% sizeFilter = 9; % Select average filter size
+% mask = maskHue(chHue(:,:,imageNumber), method, sizeFilter);
+% figure,imagesc(mask),colormap gray, title(['Final mask with method ' num2str(method)])
+% 
+% minClusterArea = 200; % Select a minimum for the cluster areas
+% detectCar(mask, imgGray(:,:,imageNumber), minClusterArea, method);
--- Lab05/include/printFigure.m
+++ Lab05/include/printFigure.m
@@ -1,32 +1,41 @@
 %% Function to print the figure 
+% Se si desidera anche salvare l'immagine, aggiungere come parametro alla
+% funzione il titolo e scommentare le ultime righe, ho aggiunto una flag
+% per dire alla stampa se fare o meno il full screen, flag = 1 sta per
+% true, altri casi è false
 
-function printFigure(numberImg, numberRow, inputImg, method, title)
-figure(), title(title);
+function printFigure(numberImg, numberRow, inputImg, method, subplotTitle, flag)
+   if flag == 1
+       figure('units','normalized','outerposition',[0 0 1 1]),
+   else 
+   end
+
    for i = 1:numberImg
        switch method{i}
             case 1 % imagesc
-                subplot(numberRow, numberImg/numberRow, i); imagesc(double(inputImg{i})); colormap gray; axis on;
+                subplot(numberRow, numberImg/numberRow, i); imagesc(double(inputImg{i})); colormap gray; title(subplotTitle{i}); axis on;
            
             case 2 % istrogramma
-                subplot(numberRow, numberImg/numberRow, i); imhist(uint8(inputImg{i}), 256); colormap gray; axis on;
+                subplot(numberRow, numberImg/numberRow, i); imhist(uint8(inputImg{i}), 256); colormap gray; title(subplotTitle{i}); axis on;
                 
             case 3 % imshow
-                subplot(numberRow, numberImg/numberRow, i); imshow(uint8(inputImg{i})); colormap gray; axis on;
+                subplot(numberRow, numberImg/numberRow, i); imshow(inputImg{i}); colormap gray; title(subplotTitle{i}); axis on;
                 
             case 4 % mesh
-                subplot(numberRow, numberImg/numberRow, i); mesh(inputImg{i}); colormap gray; axis on;
+                subplot(numberRow, numberImg/numberRow, i); mesh(inputImg{i}); colormap gray; title(subplotTitle{i}); axis on;
                 
             case 5 % surf
-                subplot(numberRow, numberImg/numberRow, i); surf(inputImg{i}); axis on;
+                subplot(numberRow, numberImg/numberRow, i); surf(inputImg{i}); title(subplotTitle{i}); axis on;
                 
             otherwise
                 disp('Error!')
             return
        end
    end
+   
    %fig = figure;
    %fname = /where/you/want/to/save
-   %figname = ['fig_', title, '.jpg'];
+   %figname = ['fig_', saveTitle, '.jpg'];
    %saveas(fig, fullfile(fname, figname), 'jpeg')
    % close(fig)
 end
