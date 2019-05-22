ecee2930c52a6c34bf76a33dce28a8c6e374fa99
Federico Tomat <39094212+federicotomat@users.noreply.github.com>
2019-05-22 15:52:30 +0200

    Lab07, controllare

---
+++ Lab07/include/my_disparity.m
@@ -0,0 +1,42 @@
+%% My disparity: computes disparity map image patches (the dimensione in computed in ssd)
+% and dmin, dmax is the range valye for disparity
+
+
+function disparityMap = my_disparity(I1, I2, W, dmin, dmax)
+    % Assuming image pair same size
+    halfW = floor(W/2);
+    drange = dmax-dmin;
+
+    offset = -dmin;
+
+    rowend = length(I1(:,1))-halfW;
+    colend = length(I1(1,:))-halfW-abs(dmax);
+
+    disparityMap = zeros(size(I1));
+    for r = halfW+1: rowend
+        for c = abs(offset)+halfW+1 : colend
+            dvec = zeros(drange,1);
+            N1 = I1(r-halfW:r+halfW, c-halfW:c+halfW);
+            for d=dmin:dmax
+
+                N2 = I2(r-halfW:r+halfW, c+d-halfW:c+d+halfW);
+                dvec(d+offset+1) = -my_ssd(N1,N2);
+
+
+            end
+            % Assign the maximum similarity point to D(r,c)
+            % If all the values of dvec are equal we choose as correspondence 
+            % point for (r,c) the point (r,c) in the other image
+            if range(dvec) == 0  
+                disparityMap(r,c) = 0;
+            else
+                [~, argmax] = max(dvec);
+                disparityMap(r,c) = argmax+dmin-1;
+            end
+        end
+    end
+end
+
+function r = range(I)
+    r = max(I(:))-min(I(:));
+end
---
+++ Lab07/include/my_ssd.m
@@ -0,0 +1,11 @@
+%% Sum of squares is a statistical technique used in regression analysis to
+% determine the dispersion of data points.
+% In a regression analysis, the goal is to determine how well a data series
+% can be fitted to a function that might help to explain how the data series 
+% was generated. 
+% Sum of squares is used as a mathematical way to find the function that 
+% best fits (varies least) from the data.
+
+function d = my_ssd(N1, N2)
+    d = sum( sum( (N1-N2).^2) );
+end
