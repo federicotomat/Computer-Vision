231a33936faf1d43bb6316b74efc99bd97cbb886
Giovanni <giovaborr@gmail.com>
2019-06-06 11:50:26 +0200

    un po di figa qua

--- Lab08/Lab08.m
+++ Lab08/Lab08.m
@@ -31,7 +31,10 @@ for i = 1:N
 end
 fprintf("The epipolar constraint with Eight Points Algorithm version 1 is: \n");
 disp(mul);
+
 visualizeEpipolarLines(img1, img2, F1, p1(1:2,:)', p2(1:2,:)')
+uiwait(helpdlg('Visualizing Epipolar 8 point Algo, not normalized, continue to go on'));
+
 [U, W, V] = svd(F1);
 rightE = U(:,end);
 leftE = V(:,end); 
@@ -44,6 +47,8 @@ end
 fprintf("The epipolar constraint with Eight Points Algorithm version 2 is: \n");
 disp(mul);
 visualizeEpipolarLines(img1, img2, F2, p1(1:2,:)', p2(1:2,:)')
+uiwait(helpdlg('Visualizing Epipolar 8 point Algo, normalized, continue to go on'));
+
 [U, W, V] = svd(F2);
 rightE = U(:,end);
 leftE = V(:,end); 
@@ -56,7 +61,7 @@ disp(rightE);
 %   correspondences to your points. Since the 8 points algorithm use them all after 
 %   a while the  estimate will be wrong.
 
-np1=0;
+np1=4;
 
 ErrMat1 = [randi([1 max(max(round(p1/2)))],2,np1); ones(1,np1)];
 ErrMat2 = [randi([1 max(max(round(p2/2)))],2,np1); ones(1,np1)];
@@ -68,19 +73,41 @@ p2Err = [p2, ErrMat2];
 %   you may have a look (and comment) at the behavior of other algorithms implemented 
 %   in Matlab (see the function estimateFundamentalMatrix)
 
-F = estimateFundamentalMatrix(p1Err(1:2,:)', p2Err(1:2,:)', 'Method','RANSAC');
+F3 = estimateFundamentalMatrix(p1Err(1:2,:)', p2Err(1:2,:)');
+F4 = estimateFundamentalMatrix(p1Err(1:2,:)', p2Err(1:2,:)', 'Method','RANSAC');
+
 
 for i = 1:N
-    mul(i) = p2(:,i)' * F * p1(:,i);
+    mul1(i) = p2(:,i)' * F3 * p1(:,i);
+    mul2(i) = p2(:,i)' * F4 * p1(:,i);
 end
 
-fprintf("The epipolar constraint with Matlab functions is: \n");
+fprintf("The epipolar constraint with Matlab functions is and no random point: \n");
 
-disp(mul);
-visualizeEpipolarLines(img1, img2, F, p1(1:2,:)', p2(1:2,:)')
-[U, W, V] = svd(F);
+disp(mul1);
+
+fprintf("The epipolar constraint with Matlab functions is and  random point: \n");
+disp(mul2)
+
+visualizeEpipolarLines(img1, img2, F3, p1(1:2,:)', p2(1:2,:)');
+uiwait(helpdlg('Visualizing MAtlab 8 point Algo,with error not RANSAC, continue to go on'));
+
+
+visualizeEpipolarLines(img1, img2, F4, p1(1:2,:)', p2(1:2,:)');
+uiwait(helpdlg('Visualizing MAtlab 8 point Algo,with error with RANSAC, continue to close'));
+
+
+[U, W, V] = svd(F3);
 rightE = U(:,end);
 leftE = V(:,end); 
-fprintf("The left and the right epipoles are:(matlab function)\n");
+fprintf("The left and the right epipoles are:(matlab function no random point)\n");
 disp(leftE);
 disp(rightE);
+
+[U, W, V] = svd(F4);
+rightE = U(:,end);
+leftE = V(:,end); 
+fprintf("The left and the right epipoles are:(matlab function and random point)\n");
+disp(leftE);
+disp(rightE);
+close all
